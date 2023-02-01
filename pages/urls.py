08c9243af27e74e47c369a60f62a50d34eb4e4b0
Nick <enviouscodefellow@outlook.com>
2023-02-01 13:28:26 -0700

    lab 33 complete

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import routers, serializers, viewsets
 from .views import my_view
+from rest_framework_simplejwt import views as jwt_views
 
 from .views import (
     AboutPageView,
@@ -16,17 +17,6 @@ from .views import (
     SnackViewSet
 )
 
-# Serializers define the API representation.
-# class UserSerializer(serializers.HyperlinkedModelSerializer):
-#     class Meta:
-#         model = User
-#         fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-# class UserViewSet(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'api', SnackViewSet)
@@ -40,9 +30,11 @@ urlpatterns = [
     path('<int:pk>/update/', SnackUpdateView.as_view(), name='update'),
     # path('create/', SnackCreateView.as_view(), name='snack_create'),
     path('<int:pk>/delete/', SnackDeleteView.as_view(), name='delete'),
-    path('api/', SnackViewSet.as_view({'get': 'list', 'post': 'create'}), name='api'),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-
     # path('<int:pk>/', SnackAPIDetailView.as_view(), name='detail'),
-    # path('', SnackAPIListView.as_view(), name='list')
+    # path('', SnackAPIListView.as_view(), name='list'),
+    path('api/', SnackViewSet.as_view({'get': 'list', 'post': 'create'}), name='api'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/token', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+
 ]
